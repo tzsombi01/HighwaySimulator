14cae6e8e792e34223b88a303f62930da4d3dd13
tzsombi01 <toreky.zsombor@gmail.com>
2022-08-30 20:01:13 +0200

    First Touches

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import time
+import pygame
 
-print("Hello world")
+HEIGHT = 500
+WIDTH = 500
+
+pygame.init()
+screen = pygame.display.set_mode((HEIGHT, WIDTH))
+pygame.display.set_caption('Highway Simulator')
+
+class Vehicle:
+	def __init__(self, typeOfVehicle, posX, posY, speed):
+		self.typeOfVehicle = typeOfVehicle
+		self.posX = posX
+		self.posY = posY
+		self.speed = speed
+
+	def move(self):
+		self.posX += self.speed
+
+	# def set_type(self, type):
+	# 	return self.typeOfVehicle = type
+
+	def get_type(self):
+		return self.typeOfVehicle
