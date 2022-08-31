9ab517cb293e58c50ce85bf54d1daafe71caf0c7
tzsombi01 <toreky.zsombor@gmail.com>
2022-08-31 10:49:22 +0200

    Added images and pygame setup

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
-import time
 import pygame
+import random
+import time
 
-HEIGHT = 500
-WIDTH = 500
+# TODO PLAY/PAUSE button
+
+GREEN = (70, 160, 0)
+WHITE = (255, 255, 255)
+
+HEIGHT = 600
+WIDTH = 1000
+
+CAR_IMAGES = [pygame.image.load("vehicles/car.png")]
+MOTORCYCLE_IMAGES = [pygame.image.load("vehicles/motorcycle.png")]
+SPEED_OF_VEHICLES = {"car": 10, "motorcycle": 15}
 
 pygame.init()
-screen = pygame.display.set_mode((HEIGHT, WIDTH))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Highway Simulator')
 
+
 class Vehicle:
+
 	def __init__(self, typeOfVehicle, posX, posY, speed):
 		self.typeOfVehicle = typeOfVehicle
 		self.posX = posX
 		self.posY = posY
 		self.speed = speed
 
+	def start(self):
+		pass
+
+	def change_lanes(self):
+		pass
+
 	def move(self):
 		self.posX += self.speed
 
-	# def set_type(self, type):
-	# 	return self.typeOfVehicle = type
-
 	def get_type(self):
 		return self.typeOfVehicle
+
+
+def pick_random_car_image():
+	return random.choice(CAR_IMAGES)
+
+
+def pick_random_motorcycle_image():
+	return random.choice(MOTORCYCLE_IMAGES)
+
+
+def draw_lanes():
+	pygame.draw.line(screen, WHITE, (0, 1 / 3 * HEIGHT), (WIDTH, 1 / 3 * HEIGHT))
+	pygame.draw.line(screen, WHITE, (0, 2 / 3 * HEIGHT), (WIDTH, 2 / 3 * HEIGHT))
+
+
+def redraw_game_window():
+	screen.fill(GREEN)
+	draw_lanes()
+	pygame.display.update()
+
+
+def main():
+	run = True
+	while run:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		redraw_game_window()
+	pygame.quit()
+
+
+if __name__ == "__main__":
+	main()
