d357aead2727dc57c79e4ade60435ea9a5ab438c
tzsombi01 <toreky.zsombor@gmail.com>
2022-09-01 09:22:19 +0200

    Making changes, fixing bugs

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import time
 
 # TODO PLAY/PAUSE button
 
+# TODO timer, multiple random lane spawn, overtaking/lane changing - also smoothing
+
+# TODO fix spawning bug - Check whether there's a vehicle in the place of the spawn
+
+# TODO resizing images
+
 GREEN = (70, 160, 0)
 WHITE = (255, 255, 255)
 
@@ -15,6 +21,7 @@ WIDTH = 1000
 
 vehicle_images = {"car": [pygame.image.load("vehicles/car.png")], "motorcycle": [pygame.image.load("vehicles/motorcycle.png")]}
 on_screen_vehicles = []
+number_of_lanes = 2
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -48,15 +55,47 @@ def draw_game_window():
 
 
 def draw_vehicle():
-	if len(on_screen_vehicles) != 0:
-		for vehicle in on_screen_vehicles:
-			# print(f"Type of vehicle: {vehicle.type_of_vehicle}, position: {vehicle.position}" )
-			screen.blit(vehicle.image_representation, (vehicle.position[0], vehicle.position[1]))
+	if len(on_screen_vehicles) == 0:
+		pass
+	for vehicle in on_screen_vehicles:
+		# print(f"Type of vehicle: {vehicle.type_of_vehicle}, position: {vehicle.position}" )
+		screen.blit(vehicle.image_representation, (vehicle.position[0], vehicle.position[1]))
 
 
 def create_vehicle():
 	type_of_vehicle = pick_random_vehicle_type()
-	on_screen_vehicles.append(Vehicle(type_of_vehicle, (0, 200), random.randint(10, 20), pick_image_representation(type_of_vehicle)))
+	on_screen_vehicles.append(Vehicle(type_of_vehicle,
+							  pick_random_lane_to_spawn(),
+							  pick_random_speed(),
+							  pick_image_representation(type_of_vehicle)))
+
+
+def pick_random_speed():
+	return random.randint(10, 20)
+
+
+# TODO refactor
+def pick_random_lane_to_spawn():
+	pos_x, pos_y = 0, 1 / random.randint(2, 3) * HEIGHT
+	tries = 0
+	threshold = number_of_lanes
+	while lane_is_occupied_at_position(pos_y):
+		if tries == threshold:
+			return WIDTH + 1, pos_y
+		pos_y = 1 / random.randint(2, 3) * HEIGHT
+		tries += 1
+	return pos_x, pos_y
+
+
+def lane_is_occupied_at_position(pos_y):
+	if len(on_screen_vehicles) == 0:
+		return False
+
+	for vehicle in on_screen_vehicles:
+		if pos_y <= vehicle.position[1] <= pos_y + vehicle.image_representation.get_width():
+			print(f"Occupied, vehicles at scene: {len(on_screen_vehicles)}")
+			return True
+	return False
 
 
 def pick_random_vehicle_type():
